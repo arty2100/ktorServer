ecec8389c52ed016da35b1021cfbc6c769710bbe
art_spb@hotmail.it <Privet2100+>
2020-05-12 16:58:50 +0200

    l

--- src/dto/PostResponseDto.kt
+++ src/dto/PostResponseDto.kt
@@ -1,17 +1,40 @@
 package com.galaktionov.dto
 
+import com.galaktionov.firstandroidapp.dto.Location
 import com.galaktionov.firstandroidapp.dto.PostModel
 
 data class PostResponseDto(
     val id: Long,
     val author: String,
-    val content: String? = null
+    val content: String? = null,
+    val created: Long,//milliseconds
+    var likedByMe: Boolean = false,
+    var likes: Int = 0,
+    var comments: Int = 0,
+    var shares: Int = 0,
+    val address: String? = null,
+    val location: Location? = null,
+    val videoUrl: String? = null,
+    val postTpe: PostModel.POST_TYPE,
+    val advLink: String? = null,
+    val companyImg: String? = null
 ) {
     companion object {
         fun fromModel(model: PostModel) = PostResponseDto(
             id = model.id,
             author = model.author,
-            content = model.content
+            content = model.content,
+            created = model.created,
+            likedByMe = model.likedByMe,
+            likes = model.likes,
+            comments = model.comments,
+            shares = model.shares,
+            address = model.address,
+            location = model.location,
+            videoUrl = model.videoUrl,
+            postTpe = model.postTpe,
+            advLink = model.advLink,
+            companyImg = model.companyImg
         )
     }
 }
