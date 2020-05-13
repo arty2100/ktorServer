1bc5973fcfac4143b2e837394be412c2ae5d1efe
art_spb@hotmail.it <Privet2100+>
2020-05-13 09:23:04 +0200

    l

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,28 +1,33 @@
 package com.galaktionov
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.features.*
-import io.ktor.gson.*
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.features.json.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.*
-import io.ktor.client.features.logging.*
-import kotlin.test.*
-import io.ktor.server.testing.*
+import com.galaktionov.firstandroidapp.dto.PostModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import io.ktor.http.ContentType
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.withCharset
+import io.ktor.server.testing.contentType
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import javafx.geometry.Pos
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 
 class ApplicationTest {
     @Test
-    fun testRoot() {
-        withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Get, "/").apply {
+    fun testGetAll() {
+        withTestApplication({ module() }) {
+            with(handleRequest(HttpMethod.Get, "/posts")) {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
+                assertEquals(
+                        ContentType.Application.Json.withCharset(Charsets.UTF_8),
+                        response.contentType()
+                )
+
+                val myType = object : TypeToken<List<PostModel>>() {}.type
+                val posts = Gson().fromJson<List<PostModel>>(response.content, myType)
             }
         }
     }
