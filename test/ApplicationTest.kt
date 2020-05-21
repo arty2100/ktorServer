3936e70fd0fb2290fa78454e9941b2eada8872f6
art_spb@hotmail.it <Privet2100+>
2020-05-21 09:40:07 +0200

    l

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -10,7 +10,6 @@ import io.ktor.http.withCharset
 import io.ktor.server.testing.contentType
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
-import javafx.geometry.Pos
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -19,7 +18,7 @@ class ApplicationTest {
     @Test
     fun testGetAll() {
         withTestApplication({ module() }) {
-            with(handleRequest(HttpMethod.Get, "/posts")) {
+            with(handleRequest(HttpMethod.Get, "/api/v1/posts")) {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals(
                         ContentType.Application.Json.withCharset(Charsets.UTF_8),
@@ -28,6 +27,7 @@ class ApplicationTest {
 
                 val myType = object : TypeToken<List<PostModel>>() {}.type
                 val posts = Gson().fromJson<List<PostModel>>(response.content, myType)
+                println(posts)
             }
         }
     }
